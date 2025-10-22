d725e9b8534ace594daad9ee3648bd5598ef246b
DeeniceVida <deenicevida@gmail.com>
2025-10-23 02:49:46 +0300

    Delete src directory

--- src/pages/Blogs.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-export default function Blogs(){
-  return (
-    <div>
-      <h1 className="text-2xl font-bold">Blogs</h1>
-      <p className="mt-4">This project uses a /blogs folder with markdown files for Jekyll. See README for instructions to setup Jekyll and how to add posts.</p>
-    </div>
-  )
-}
